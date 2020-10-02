8c1c0f2d3bec2a014245ed30024101a55c90c497
Fedor <qwertyfredya@gmail.com>
2020-10-02 20:37:46 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+// webpack.config.js
+const path = require('path');
+// подключаем path к конфигу вебпак
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Подключили к проекту плагин
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: { main: './src/index.js' }, // Точка входа
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Точка выхода
+    filename: 'main.js'
+  },
+  module: {
+    rules: [{ // тут описываются правила
+      test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+      use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
+      exclude: /node_modules/ // исключает папку node_modules
+    },
+    {
+      test: /\.css$/, // применять это правило только к CSS-файлам
+      use: [MiniCssExtractPlugin.loader, 'css-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
+    }
+    ]
+  },
+  plugins: [ 
+    new MiniCssExtractPlugin({filename: 'style.css'}),
+    new HtmlWebpackPlugin({
+      // Означает, что:
+      inject: false, // стили НЕ нужно прописывать внутри тегов
+      hash: true, // для страницы нужно считать хеш
+      template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
+      filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
+    })
+    ]
+}
+// переписали точку выхода, используя утилиту path
